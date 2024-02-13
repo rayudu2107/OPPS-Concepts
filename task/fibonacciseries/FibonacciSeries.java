d06dd096d3e5f7a4b2bf731c8afaec0f9c9042a1
Pradeepv@123 <pradeepkumer2107@gmail.com>
2024-02-13 15:56:51 +0530

    Task-2, Fibbonacci series upto a specified no. of terms

---
+++ task/fibonacciseries/FibonacciSeries.java
@@ -0,0 +1,18 @@
+package task.fibonacciseries;
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n, f1 = 0, f2 = 1;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        n = s.nextInt();
+        System.out.print("Fibonacci Series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(f1 + " ");
+            int sum = f1 + f2;
+            f1 = f2;
+            f2 = sum;
+        }
+    }
+}
